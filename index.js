dc0cede59df14684715657daaed5894b6fd8bdf3
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-07-22 22:12:40 +0530

    created api route for making shorturl

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const URL = require('url').URL
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// for now we just use local variable to store the data instead of mongodb
+const arr = [];
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -21,6 +24,25 @@ app.get('/api/hello', function(req, res) {
 });
 
 // for doing this url shortner task we use body-parser to parse the body and also url
+
+app.post("/api/shorturl", (req, res) => {
+  console.log(req.body, " ------- req.body");
+  // let originalURL = "www.example.com";
+  // console.log("Hello");
+  // console.log(urlObject.hostname, " ========= 1");
+  // console.log(urlObject, " ------------- 2");
+  // );
+  try {
+    const urlObject = new URL(req.body.url);
+    arr.push({ originalUrl: req.body.url });
+    res.json({
+      original_url: req.body.url,
+      short_url: arr.length,
+    });
+  } catch (err) {
+    res.json({ error: "Invalid URL" });
+  }
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
